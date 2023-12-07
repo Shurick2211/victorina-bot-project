1ea78cdf52155f097b19bdfef6a15daa4f58b332
User <Shurick2211@gmail.com>
2023-12-07 22:05:23 +0200

    stat + all participants

--- admin-ui/src/app/components/statistic/statistic.component.ts
+++ admin-ui/src/app/components/statistic/statistic.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {StorageService} from "../../services/storage.service";
 import {Victorina} from "../../dto/victorina";
 import {ActivatedRoute} from "@angular/router";
@@ -11,22 +11,26 @@ import {Person} from "../../dto/person";
   templateUrl: './statistic.component.html',
   styleUrl: './statistic.component.scss'
 })
-export class StatisticComponent {
+export class StatisticComponent{
+
   id:number
+
   victorina:Victorina
-  panelOpenState = true
+
+  sobaka='@'
 
   winner:Person | null = null
   owner:Person | null = null
   isAdmin = this.storage.person?.role === PersonRole.ADMIN
 
   panelOpenStatePart= false
-
+  panelOpenState = false
   constructor(protected storage:StorageService, private api:ApiService,activeRoute:ActivatedRoute) {
     this.id = activeRoute.snapshot.params['id']
     this.victorina = storage.victorinas[this.id]
-    //this.victorina = storage.victorinas.filter( v => v.id === id ).shift() as Victorina
+
   }
+
 
   getOwner(){
     this.api.getPerson(this.victorina.ownerId).subscribe( response => {
@@ -54,5 +58,13 @@ export class StatisticComponent {
     return this.persons.find(p => p.id === id)
   }
 
+  getPersonResult(id:string): number | undefined {
+    return this.persons.find(p => p.id === id)?.quizes.find(q => q.victorinaId === this.victorina.id)?.percentRightAnswer
+  }
 
+  getParticipantsPoll() {
+    this.victorina.participantsId?.forEach(id=> {
+      this.addPerson(id.toString())
+    })
+  }
 }
--- admin-ui/src/app/dto/quiz.ts
+++ admin-ui/src/app/dto/quiz.ts
@@ -3,7 +3,8 @@ export class Quiz {
   constructor(
     public victorinaId:String,
     public userAnswers:Array<number>,
-    public isWinner:Boolean
+    public isWinner:Boolean,
+    public percentRightAnswer:number
   ) {
   }
 }
