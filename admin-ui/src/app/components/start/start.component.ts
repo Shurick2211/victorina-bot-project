58d8baf9de2e4575be2fe2ef5a6ed182ae64a500
User <Shurick2211@gmail.com>
2023-12-10 19:51:02 +0200

    mess of start page

--- admin-ui/src/app/components/start/start.component.ts
+++ admin-ui/src/app/components/start/start.component.ts
@@ -11,6 +11,14 @@ export class StartComponent implements OnInit{
   hide = true;
 
   error:string|null = null
+  rules = "If you created quiz for channel with prize, in day of start in 8:00 GMT, " +
+    "in your channel the bot send message with name of your quiz and button for start the quiz. " +
+    "The bot will only allow users subscribed to the channel to participate in the quiz. " +
+    "If user is`n subscribed - the bot send channel`s link for subscribe, and after it allow " +
+    "to participate in the quiz!\n" +
+    "The day after end date in 10:00 GMT the bot will choose a winner randomly among those who right answered!" +
+    "And all of them will received message with winner`s name! After it the winner will be asked for the delivery " +
+    "address, and the bot will forward information about the winner and his address to the owner of the quiz."
 
   constructor(public storage:StorageService) {
     storage.refreshPerson()
