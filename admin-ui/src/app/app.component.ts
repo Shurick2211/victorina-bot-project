415e1c6cdfc66f9b2bc38f44fd22a3bf53edd31f
Aleksandr Nimko <shurick2211@gmail.com>
2023-12-11 12:01:30 +0200

    change adminka style++

--- admin-ui/src/app/app.component.ts
+++ admin-ui/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject, OnInit, signal} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {DOCUMENT} from "@angular/common";
 import {Router} from "@angular/router";
 import {StorageService} from "./services/storage.service";
